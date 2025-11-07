af952e14e4258c50c8e9d40553f8e15bacf14f83
thebestmexican09-web <thebestmexican09@gmail.com>
2025-11-07 12:24:52 -0800

    Update basic.cy.js

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,13 +4,13 @@ describe("empty spec", () => {
   });
 
   it("opens the index page", () => {
-    cy.get("h1").contains("Great coffee with a conscience");
+    cy.get("h1").contains("Making families smile all across the world");
   });
 
   it("navigates to the product page", () => {
     cy.get('a[href="/products"]').eq(0).click();
     cy.url().should("include", "/products");
-    cy.get("h1").contains(/Our Coffee/i);
+    cy.get("h1").contains(/Our quilts/i);
   });
 
   it("navigates to the values page", () => {
